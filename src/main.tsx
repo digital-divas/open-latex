a33e7fcc45dac0ec5e0c7b3e3ba18dd251d6d9e9
Ezequiel Ramos <ezequielmr94@gmail.com>
2025-10-26 15:52:43 -0300

    vambora

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/open-latex/sw.js')
+      .catch(err => console.error('error on registering SW', err));
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
